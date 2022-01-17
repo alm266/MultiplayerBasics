c34db07d5089cee096253e94f55ce019f0a5682e
Andrew Marshall <andrewleonmarshall@gmail.com>
2022-01-17 17:03:22 -0500

    Players can connect and move

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDisplayName($"Player {numPlayers}");
+
+        player.SetColor(new Color(Random.value, Random.value, Random.value));
+    }
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,71 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
+
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))]
+    [SerializeField]
+    private string displayName = "Missing Name";
+
+    [SyncVar(hook = nameof(HandleDisplayColorUpdated))]
+    [SerializeField]
+    private Color displayColor;
+
+    #region Server
+
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        if(newDisplayName.Length > 10) { return; }
+
+        displayName = newDisplayName;
+    }
+
+    [Server]
+    public void SetColor(Color color)
+    {
+        displayColor = color;
+    }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        RpcLogNewName(newDisplayName);
+
+        SetDisplayName(newDisplayName);
+    }
+
+    #endregion
+
+    #region Client
+
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.text = newName;
+    }
+
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+    [ContextMenu("Set My Name")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("My New Name");
+    }
+
+    [ClientRpc]
+    private void RpcLogNewName(string newDisplayName)
+    {
+        Debug.Log(newDisplayName);
+    }
+
+    #endregion
+}
